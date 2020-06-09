dddb57e4155676a75bb2eb49b0401f7cf59dea66
Bahn <Bjoern.Bahn@dlr.de>
2020-06-09 12:08:19 +0200

    osi headers usable in OSIMapper, but precompiled osilibs used

--- include/mapper/OSIMapper.h
+++ include/mapper/OSIMapper.h
@@ -1,5 +1,15 @@
 #ifndef OSIMAPPER_H
 #define OSIMAPPER_H
 
+#include "Mapper.h"
+#include "open-simulation-interface/build/osi_environment.pb.h"
+
+class OSIMapper : Mapper {
+
+public:
+	OSIMapper() : Mapper() {};
+	int readConfiguration(configVariants_t config) override;
+
+};
 
 #endif // !OSIMAPPER_H
--- src/mapper/OSIMapper.cpp
+++ src/mapper/OSIMapper.cpp
@@ -1,1 +1,5 @@
 #include "mapper/OSIMapper.h"
+
+int OSIMapper::readConfiguration(configVariants_t config) {
+	return 0;
+}
