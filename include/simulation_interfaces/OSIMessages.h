4c9d2a69c1a90e98dadf6c8ba4bb18e9dca3bdfc
Bahn <Bjoern.Bahn@dlr.de>
2020-06-30 10:31:20 +0200

    remove splitting of osi messages in OSIMapper, splittin shall be handled by applications accessing the data or base interface implementation

--- include/simulation_interfaces/OSIMessages.h
+++ include/simulation_interfaces/OSIMessages.h
@@ -11,44 +11,13 @@
 * Implemented OSI Messages
 */
 enum eOSIMessage {
-	//Sensorview
-	SensorViewMessage,
 
-	GenericSensorViewMessage,
-	RadarSensorViewMessage,
-	LidarSensorViewMessage,
-	CameraSensorViewMessage,
-	UltrasonicSensorViewMessage,
-	
-	//SensorViewConfiguration
+	SensorViewMessage,	
 	SensorViewConfigurationMessage,
-
-	GenericSensorViewConfigurationMessage,
-	RadarSensorViewConfigurationMessage,
-	LidarSensorViewConfigurationMessage,
-	CameraSensorViewConfigurationMessage,
-	UltrasonicSensorViewConfigurationMessage,
-
-	//SensorData
 	SensorDataMessage,
-
-	//GroundTruth
 	GroundTruthMessage,
-
-	StationaryObjectMessage,
-	MovingObjectMessage,
-	TrafficSignMessage,
-	TrafficLightMessage,
-	RoadMarkingMessage,
-	LaneBoundaryMessage,
-	LaneMessage,
-	OccupantMessage,
-
-	//TrafficCommand (SL45)
 	SL45TrafficCommandMessage,
-
-	//InVehicleSensorData (SL45?)
-	SL45InVehicleSensorDataMessage,
+	SL45InVehicleSensorDataMessage
 };
 
 /**
@@ -69,38 +38,11 @@ struct address {
 * Type definition of std::variant collection of all implemented OSI messages.
 */
 typedef std::variant<
-	//SensorView
 	osi3::SensorView,
-	osi3::GenericSensorView,
-	osi3::RadarSensorView,
-	osi3::LidarSensorView,
-	osi3::CameraSensorView,
-	osi3::UltrasonicSensorView,
-
-	//SensorViewConfiguration
 	osi3::SensorViewConfiguration,
-	osi3::GenericSensorViewConfiguration,
-	osi3::RadarSensorViewConfiguration,
-	osi3::LidarSensorViewConfiguration,
-	osi3::CameraSensorViewConfiguration,
-	osi3::UltrasonicSensorViewConfiguration,
-
-	//SensorData
 	osi3::SensorData,
-
-	//GroundTruth
-	osi3::GroundTruth,
-	osi3::StationaryObject,
-	osi3::MovingObject,
-	osi3::TrafficSign,
-	osi3::TrafficLight,
-	osi3::RoadMarking,
-	osi3::LaneBoundary,
-	osi3::Lane,
-	osi3::Occupant
-
+	osi3::GroundTruth
 	//TrafficCommand (SL45)
-
 	//InVehicleSensorData (SL45?)
 
 > osiMessage_t;
