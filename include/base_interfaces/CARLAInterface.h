b2b251a388e8a79ca7e136d4b85fbee62dd4d315
Nils Wendorff <Nils.Wendorff@dlr.de>
2020-07-21 21:05:25 +0200

    Add OSI headers and some stubs to Carla base interface

--- include/base_interfaces/CARLAInterface.h
+++ include/base_interfaces/CARLAInterface.h
@@ -7,6 +7,8 @@
 #include "configreader/BaseConfigVariants.h"
 
 #include <carla/client/ActorBlueprint.h>
+#include <carla/client/ActorList.h>
+#include <carla/client/Actor.h>
 #include <carla/client/BlueprintLibrary.h>
 #include <carla/client/Client.h>
 #include <carla/client/Map.h>
@@ -18,13 +20,42 @@
 #include <carla/image/ImageView.h>
 #include <carla/sensor/data/Image.h>
 
+#include "osi_common.pb.h"
+#include "osi_datarecording.pb.h"
+#include "osi_detectedlane.pb.h"
+#include "osi_detectedobject.pb.h"
+#include "osi_detectedoccupant.pb.h"
+#include "osi_detectedroadmarking.pb.h"
+#include "osi_detectedtrafficlight.pb.h"
+#include "osi_detectedtrafficsign.pb.h"
+#include "osi_environment.pb.h"
+#include "osi_featuredata.pb.h"
+#include "osi_groundtruth.pb.h"
+#include "osi_hostvehicledata.pb.h"
+#include "osi_lane.pb.h"
+#include "osi_object.pb.h"
+#include "osi_occupant.pb.h"
+#include "osi_roadmarking.pb.h"
+#include "osi_sensordata.pb.h"
+#include "osi_sensorspecific.pb.h"
+#include "osi_sensorview.pb.h"
+#include "osi_sensorviewconfiguration.pb.h"
+#include "osi_trafficlight.pb.h"
+#include "osi_trafficsign.pb.h"
+#include "osi_version.pb.h"
+
+
 class CARLAInterface : public BaseSystemInterface
 {
 	std::string host;
 	uint16_t port;
 	std::unique_ptr<carla::client::World> world;
 	std::unique_ptr<carla::client::Client> client;
+	std::map<std::string, carla::ActorId> actorRole2IDMap;
+	std::set<carla::ActorId> activeActors;
 	carla::time_duration transactionTimeout;
+	double deltaSeconds;
+	std::shared_ptr<osi3::GroundTruth> mapTruth;
 
 public:
 
@@ -57,6 +88,15 @@ public:
 	virtual int setDoubleValue(std::string base_name, double value) override;
 	virtual int setStringValue(std::string base_name, std::string value) override;
 
+private:
+
+	std::string_view getPrefix(std::string_view name);
+	osi3::Timestamp parseTimestamp();
+	// prepare a GroundTruth object with values from the current map which won't change 
+	//TODO return type
+	void parseStationaryMapObjects();
+	osi3::GroundTruth parseWorldToGroundTruth();
+	std::vector<osi3::SensorView> parseSensorActors();
 
 };
 
--- src/base_interfaces/CARLAInterface.cpp
+++ src/base_interfaces/CARLAInterface.cpp
@@ -20,11 +20,46 @@ int CARLAInterface::initialise() {
 	this->client = std::make_unique<carla::client::Client>(host, port);
 	this->client->SetTimeout(transactionTimeout);
 	this->world = std::make_unique<carla::client::World>(std::move(client->GetWorld()));
+
+	//assure server is in synchronous mode
+	auto settings = world->GetSettings();
+	settings.fixed_delta_seconds = this->deltaSeconds;
+	settings.synchronous_mode = true;
+	this->world->ApplySettings(settings);
+
+	//get all actors and their role (if set)
+	for each (auto actor in *world->GetActors())
+	{
+		activeActors.insert(actor->GetId());
+		auto attributes = actor->GetAttributes();
+		for each (auto attribute in attributes) {
+			if ("role_name" == attribute.GetId() && !attribute.GetValue().empty()) {
+				actorRole2IDMap.try_emplace(attribute.GetValue(), actor->GetId());
+			}
+		}
+	}
+
+	parseStationaryMapObjects();
+
 	return 0;
 }
 
 double CARLAInterface::doStep() {
-	return 0;
+	if (!world) {
+		throw std::exception("No world");
+	}
+
+
+	world->Tick(this->transactionTimeout);
+	//world->WaitForTick(this->transactionTimeout);
+
+	std::vector<carla::ActorId> removedActors;
+	auto worldActors = world->GetActors();
+	//std::sort(worldActors->begin(), worldActors->end());
+	//std::set_difference(activeActors.begin(), activeActors.end(), worldActors->begin(), worldActors->end(), removedActors.begin());
+	//std::sort(activeActors.begin(), activeActors.end());
+
+	return this->deltaSeconds;
 }
 
 int CARLAInterface::getIntValue(std::string base_name) {
@@ -44,6 +79,10 @@ double CARLAInterface::getDoubleValue(std::string base_name) {
 };
 
 std::string CARLAInterface::getStringValue(std::string base_name) {
+
+
+
+
 	return "";
 };
 
@@ -65,4 +104,112 @@ int CARLAInterface::setDoubleValue(std::string base_name, double value) {
 
 int CARLAInterface::setStringValue(std::string base_name, std::string value) {
 	return 0;
-};
+}
+std::string_view CARLAInterface::getPrefix(std::string_view name)
+{
+	if (2 < name.size() && '#' == name[0]) {
+		std::string_view prefix = std::string_view(&name.at(1), name.find('#', 1));
+		return prefix;
+	}
+	return std::string_view();
+}
+
+
+osi3::Timestamp CARLAInterface::parseTimestamp()
+{
+	osi3::Timestamp osiTime;
+	auto carlaTime = world->GetSnapshot().GetTimestamp();
+	double intPart;
+	double fractional = std::modf(carlaTime.elapsed_seconds, &intPart);
+	osiTime.set_seconds(google::protobuf::int64(intPart));
+	osiTime.set_nanos(google::protobuf::uint32(fractional *1e9));
+	return osiTime;
+}
+
+
+void CARLAInterface::parseStationaryMapObjects()
+{
+	if (mapTruth) {
+		mapTruth->Clear();
+	}
+	else {
+		mapTruth = std::make_shared<osi3::GroundTruth>();
+	}
+
+	carla::SharedPtr<carla::client::Map> map = world->GetMap();
+	//TODO parse map parts that won't change during simulation
+
+	mapTruth->set_map_reference(map->GetName());
+
+
+	//TODO might be able to get lanes using map->GetTopology() and next_until_lane_end and previous_until_lane_start
+
+}
+
+osi3::GroundTruth CARLAInterface::parseWorldToGroundTruth()
+{
+
+	// lanes and lane boundaries are part of the map, which shouldn't change during simulation and can be preparsed during init
+	// use mapTruth as a base for every new groundTruth message that already contains unchanging fields
+	osi3::GroundTruth groundTruth(*mapTruth);
+	//groundTruth.MergeFrom(mapTruth);
+
+	for each (auto actor in *world->GetActors()) {
+		auto typeID = actor->GetTypeId();
+
+		//based on blueprint vehicle.*
+		if (typeID.rfind("vehicle", 0) == 0) {
+			auto vehicle = groundTruth.add_moving_object();
+			auto vehicleActor = boost::static_pointer_cast<carla::client::Vehicle>(actor);
+			//TODO parse vehicle as moving object
+
+
+
+
+			//TODO should walkers be parsed as moving objects?
+		}
+	}
+
+
+
+	return groundTruth;
+}
+
+std::vector<osi3::SensorView> CARLAInterface::parseSensorActors()
+{
+	std::vector<osi3::SensorView> sensorViews;
+	for each (auto actor in *world->GetActors()) {
+		auto typeID = actor->GetTypeId();
+
+
+
+		//based on blueprint sensor.*
+		if (0 == typeID.rfind("sensor", 0))
+		{
+			osi3::SensorView sensorView;
+			auto sensor = boost::static_pointer_cast<carla::client::Sensor>(actor);
+
+			//substring of typeID
+			std::string_view sensorType(&typeID[7]);
+
+			if (0 == sensorType.rfind("camera.rgb", 0))
+			{
+
+			}
+			else if (0 == sensorType.rfind("lidar.ray_cast", 0))
+			{
+
+			}
+			else if (0 == sensorType.rfind("other.radar", 0))
+			{
+
+			}
+
+
+
+
+			//TODO parse sensor view depending on typeID
+		}
+	}
+	return sensorViews;
+}
