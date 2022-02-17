83568de20179cd5a3dfa6fdb63b4a646c5050590
bahn_bj <bjoern.bahn@dlr.de>
2022-02-17 15:44:17 +0100

    without static objects in groundtruth

--- src/CARLA2OSIInterface.cpp
+++ src/CARLA2OSIInterface.cpp
@@ -507,7 +507,7 @@ std::shared_ptr<osi3::GroundTruth> CARLA2OSIInterface::parseWorldToGroundTruth()
 	// lanes and lane boundaries are part of the map, which shouldn't change during simulation and can be preparsed during init
 	// use staticMapTruth as a base for every new groundTruth message that already contains unchanging fields
 	std::shared_ptr<osi3::GroundTruth> groundTruth = std::make_shared<osi3::GroundTruth>();
-	groundTruth->MergeFrom(*staticMapTruth);
+	//groundTruth->MergeFrom(*staticMapTruth);
 
 	auto worldActors = world->GetActors();
 	for (auto actor : *worldActors) {
